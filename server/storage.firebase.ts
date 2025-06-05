7955a977c060ab58e3d6eff99dd96103fc9d915e
IGTECH <igorhawkingg@gmail.com>
2025-06-05 14:50:51 -0300

    Update storage.firebase.ts

--- server/storage.firebase.ts
+++ server/storage.firebase.ts
@@ -6,13 +6,14 @@ import type {
 } from '@shared/schema';
 import type { IStorage } from './storage';
 import * as fs from 'fs';
+const path = require('path');
 
-const serviceAccountPath = 'server/credentials/serviceAccountKey.json';
+const serviceAccountPath = path.join(__dirname, 'credentials', 'serviceAccountKey.json');
 if (!fs.existsSync(serviceAccountPath)) {
   throw new Error('Arquivo de credenciais do Firebase não encontrado: ' + serviceAccountPath);
 }
 
-const serviceAccount = require('../../' + serviceAccountPath);
+const serviceAccount = require(serviceAccountPath);
 
 let app: App;
 if (!getApps().length) {
